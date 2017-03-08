01aed8b88a9ebcaaf1452ebcf6c6960122902b59
Rob Brentnall <rob.brentnall@gmail.com>
2017-03-08 14:00:51 +0000

    slight refactor

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -17,22 +17,19 @@ class GildedRose
     item.sell_in -= 1
     return item.quality = 0 if item.sell_in <= 0
     return if item.quality == 50
-    item.quality += 1
-    item.quality += 1 if item.sell_in <= 10
+    item.sell_in <= 10 ? item.quality += 2 : item.quality += 1
     item.quality += 1 if item.sell_in <= 5
   end
 
   def update_brie(item)
     item.sell_in -= 1
     return if item.quality == 50
-    item.quality += 1
-    item.quality += 1 if item.sell_in <= 0
+    item.sell_in <= 0 ? item.quality += 2 : item.quality += 1
   end
 
   def update_normal(item)
     item.sell_in -= 1
-    item.quality -= 1
-    item.quality -= 1 if item.sell_in <= 0
+    item.sell_in <= 0 ? item.quality -= 2 : item.quality -= 1
   end
 
 end
